8a70cb8b1b2f8e88b57532d09e49c4e56fea31f0
Henry Saint Juste <hbstjuste@gmail.com>
2020-12-07 17:35:00 -0700

    Create cityWeather schema and model.

---
+++ models/cityWeather.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { CurrentWeatherSchema } = require("./currentWeather");
+const { DailyWeatherSchema } = require("./dailyWeather");
+const { HourlyWeatherSchema } = require("./hourlyWeather");
+
+const CityWeatherSchema = new mongoose.Schema({
+  current: CurrentWeatherSchema,
+  daily: DailyWeatherSchema,
+  hourly: HourlyWeatherSchema,
+  location: String,
+  alerts: Array,
+});
+
+const CityWeather = mongoose.model("city-weather", CityWeatherSchema);
+
+module.exports.CityWeather = CityWeather;
+module.exports.CityWeatherSchema = CityWeatherSchema;
