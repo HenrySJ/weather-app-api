6c2a16297ba47149ebec3e2d67273f8cf2a19c55
Henry Saint Juste <hbstjuste@gmail.com>
2020-12-17 14:56:44 -0700

    Add get route for weather.

--- routes/weather.js
+++ routes/weather.js
@@ -3,20 +3,94 @@ const router = express.Router();
 const axios = require("axios");
 const config = require("config");
 const { CityWeather } = require("../models/cityWeather");
+const Joi = require("joi");
+const { CurrentWeather } = require("../models/currentWeather");
+const { DailyWeather } = require("../models/dailyWeather");
+const { date } = require("joi");
+const { HourlyWeather } = require("../models/hourlyWeather");
+
+const schema = Joi.object({
+  lat: Joi.number().required(),
+  lon: Joi.number().required(),
+});
 
 router.get("/", async (req, res) => {
-  const coord = {
+  const { error } = schema.validate({
     lat: req.query.lat,
     lon: req.query.lon,
-  };
+  });
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
 
   try {
     const { data } = await axios.get(
-      `${config.get("URL")}lat=${coord.lat}&lon=${
-        coord.lon
+      `${config.get("URL")}lat=${req.query.lat}&lon=${
+        req.query.lon
       }&units=imperial&appid=${config.get("API_KEY")}`
     );
-    res.send(data);
+
+    const geo = await axios.get(
+      `https://geocode.xyz/${data.lat},${data.lon}?json=1`
+    );
+
+    const newCurrentWeather = new CurrentWeather({
+      icon: data.current.weather[0].icon,
+      temp: data.current.temp,
+      date: data.current.dt,
+      data: {
+        forcast: data.current.weather[0].forcast,
+        wind: {
+          windDegree: data.current.wind_deg,
+          windSpeed: data.current.wind_speed,
+        },
+        humidity: data.current.humidity,
+        visibility: data.current.visibility,
+        sunrise: data.current.sunrise,
+        sunset: data.current.sunset,
+      },
+    });
+
+    const dailyArray = data.daily.map((current) => {
+      return {
+        icon: current.weather[0].icon,
+        temp: current.temp.max,
+        date: current.dt,
+        data: {
+          forecast: current.weather[0].description,
+          wind: {
+            windDegree: current.wind_deg,
+            windSpeed: current.wind_speed,
+          },
+          humidity: current.humidity,
+          sunrise: current.sunrise,
+          sunset: current.sunset,
+        },
+      };
+    });
+    const newDailyWeather = new DailyWeather({
+      data: dailyArray,
+    });
+
+    const newHourlyWeahter = new HourlyWeather({
+      data: data.hourly,
+    });
+
+    const newCityWeather = new CityWeather({
+      coordinates: {
+        lat: data.lat,
+        lon: data.lon,
+      },
+      current: newCurrentWeather,
+      daily: newDailyWeather,
+      hourly: newHourlyWeahter,
+      location: geo.data.city,
+      alerts: data.alerts,
+    });
+    const weather = await newCityWeather.save();
+    res.send(weather);
   } catch (error) {
     res.send(error.message);
   }
