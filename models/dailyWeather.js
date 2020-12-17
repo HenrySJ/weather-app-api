3063978159ed2d00baa261dc4b26de9df81eb7f1
Henry Saint Juste <hbstjuste@gmail.com>
2020-12-17 14:53:51 -0700

    Fix DailyWeatherScema.

--- models/dailyWeather.js
+++ models/dailyWeather.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 
 const DailyWeatherSchema = new mongoose.Schema({
-  icon: String,
-  temp: Number,
-  date: Number,
-  data: {
-    forecast: String,
-    wind: { windDegree: Number, windSpeed: Number },
-    humidity: Number,
-    sunrise: Number,
-    sunset: Number,
-  },
+  data: [
+    {
+      icon: String,
+      temp: Number,
+      date: Number,
+      data: {
+        forecast: String,
+        wind: { windDegree: Number, windSpeed: Number },
+        humidity: Number,
+        sunrise: Number,
+        sunset: Number,
+      },
+    },
+  ],
 });
 
 const DailyWeather = mongoose.model("daily-weather", DailyWeatherSchema);
