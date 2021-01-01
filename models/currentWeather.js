3b3da33d1fb188cca5f3c28aaa46d099cbd11059
Henry Saint Juste <hbstjuste@gmail.com>
2020-12-31 21:48:23 -0700

    change to Date type in currentWeather model.

--- models/currentWeather.js
+++ models/currentWeather.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const CurrentWeatherSchema = new mongoose.Schema({
   icon: String,
   temp: Number,
-  date: Number,
+  date: Date,
   data: {
     forcast: String,
     wind: { windDegree: Number, windSpeed: Number },
     humidity: Number,
     visibility: Number,
-    sunrise: Number,
-    sunset: Number,
+    sunrise: Date,
+    sunset: Date,
   },
 });
 
