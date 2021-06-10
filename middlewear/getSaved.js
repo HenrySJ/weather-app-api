b3063473c99d0e5fb4714861332caee9da39b2f6
Henry Saint Juste <hbstjuste@gmail.com>
2021-06-10 12:44:48 -0700

    replace nested query logic for city weather

--- middlewear/getSaved.js
+++ middlewear/getSaved.js
@@ -1,22 +1,22 @@
-const { CityWeather } = require("../models/cityWeather");
+const { CityWeather } = require('../models/cityWeather')
 
 const getSaved = async (req) => {
   const weather = await CityWeather.findOne({
-    "coordinates.lat": `${req.query.lat}`,
-    "coordinates.lon": `${req.query.lon}`,
-  });
+    lat: `${req.query.lat}`,
+    lon: `${req.query.lon}`,
+  })
 
-  if (!weather) return "";
+  if (!weather) return ''
 
-  if (compareTimestamp(weather._id.getTimestamp())) return weather;
+  if (compareTimestamp(weather._id.getTimestamp())) return weather
 
-  await CityWeather.deleteOne({ _id: weather._id });
-  return "";
-};
-
-function compareTimestamp(timestamp) {
-  const isodate = new Date(new Date().toISOString());
-  if (isodate.getTime() - timestamp.getTime() < 3000) return true;
+  await CityWeather.deleteOne({ _id: weather._id })
+  return ''
 }
 
-module.exports.getSaved = getSaved;
+function compareTimestamp(timestamp) {
+  const isodate = new Date(new Date().toISOString())
+  if (isodate.getTime() - timestamp.getTime() < 3000) return true
+}
+
+module.exports.getSaved = getSaved
